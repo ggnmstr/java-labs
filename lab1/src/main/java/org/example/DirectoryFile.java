1488de35fe278c78d2e9140cab3cc77f3f4733da
ggnmstr <jora.babayan@gmail.com>
2023-03-06 19:03:33 +0700

    removed unused code

--- lab1/src/main/java/org/example/DirectoryFile.java
+++ lab1/src/main/java/org/example/DirectoryFile.java
@@ -8,37 +8,8 @@ import java.util.Collections;
 import java.util.List;
 
 public class DirectoryFile extends File {
-    // Cross CR: List<File>
-    private ArrayList<File> children;
 
-    /*
-    public DirectoryFile(Path path) {
-        this.path = path;
-        this.size = 0;
-        children = new ArrayList<>();
-        try {
-            List<Path> inside = Files.list(path).toList();
-            for (Path x : inside) {
-                if (Files.isRegularFile(x)) {
-                    children.add(new RegularFile(x));
-                } else if (Files.isDirectory(x) && !Files.isSymbolicLink(x) ) {
-                    children.add(new DirectoryFile(x));
-                } else if (Files.isSymbolicLink(x)) {
-                    //System.out.println(x.toRealPath());
-                    children.add(new SymlinkFile(x));
-                }
-                //children.add(Files.isRegularFile(x) ? new RegularFile(x) : new DirectoryFile(x));
-            }
-        } catch (IOException e) {
-            System.err.println("oops");
-        }
-        for (File x : children) {
-            size += x.getSize();
-        }
-        Collections.sort(children, Collections.reverseOrder());
-    }
-
-     */
+    private List<File> children;
 
     public DirectoryFile(Path path) {
         super(path);
