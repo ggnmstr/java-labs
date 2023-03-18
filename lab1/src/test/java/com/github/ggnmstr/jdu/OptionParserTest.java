138143a5fc24b0cca0ed9bc9a34a11d2abd31fb2
ggnmstr <jora.babayan@gmail.com>
2023-03-18 17:16:44 +0700

    swapped positions of excepted and actual arguments in OptionParserTest to follow signature

--- lab1/src/test/java/com/github/ggnmstr/jdu/OptionParserTest.java
+++ lab1/src/test/java/com/github/ggnmstr/jdu/OptionParserTest.java
@@ -8,28 +8,28 @@ public class OptionParserTest {
     public void testDefaultParameters() throws ParserException {
         String[] args = {"/foo/bar"};
         Options options = OptionsParser.parseParams(args);
-        TestCase.assertEquals(options,new Options(5,5,false));
+        TestCase.assertEquals(new Options(5,5,false),options);
     }
 
     @Test
     public void testSymlinkKey() throws ParserException {
         String[] args = {"-L","/foo/bar"};
         Options options = OptionsParser.parseParams(args);
-        TestCase.assertEquals(options,new Options(5,5,true));
+        TestCase.assertEquals(new Options(5,5,true),options);
     }
 
     @Test
     public void testDepth() throws ParserException {
         String[] args = {"--depth","10","/foo/bar"};
         Options options = OptionsParser.parseParams(args);
-        TestCase.assertEquals(options,new Options(10,5,false));
+        TestCase.assertEquals(new Options(10,5,false),options);
     }
 
     @Test
     public void testLimit() throws ParserException {
         String[] args = {"--limit","10","/foo/bar"};
         Options options = OptionsParser.parseParams(args);
-        TestCase.assertEquals(options,new Options(5,10,false));
+        TestCase.assertEquals(new Options(5,10,false),options);
     }
 
     @Test
@@ -38,15 +38,15 @@ public class OptionParserTest {
 
         String[] args = {"--limit","2","--depth","7","-L","/foo/bar"};
         Options options = OptionsParser.parseParams(args);
-        TestCase.assertEquals(options,excepted);
+        TestCase.assertEquals(excepted,options);
 
         args = new String[]{"--depth", "7", "--limit", "2", "-L", "/foo/bar"};
         options = OptionsParser.parseParams(args);
-        TestCase.assertEquals(options,excepted);
+        TestCase.assertEquals(excepted,options);
 
         args = new String[]{"--depth","7","-L", "--limit","2","/foo/bar"};
         options = OptionsParser.parseParams(args);
-        TestCase.assertEquals(options,excepted);
+        TestCase.assertEquals(excepted,options);
     }
 
     @Test
