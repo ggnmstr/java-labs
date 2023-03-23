1288f9624b5ebd499402201ebb1fcd13adcdd723
ggnmstr <jora.babayan@gmail.com>
2023-03-23 17:56:34 +0700

    remove line

--- lab1/src/main/java/com/github/ggnmstr/jdu/exception/DuParserException.java
+++ lab1/src/main/java/com/github/ggnmstr/jdu/exception/DuParserException.java
@@ -1,6 +1,5 @@
 package com.github.ggnmstr.jdu.exception;
 
-// CR: unchecked
 public class DuParserException extends RuntimeException {
     public DuParserException(String message) {
         super(message);
