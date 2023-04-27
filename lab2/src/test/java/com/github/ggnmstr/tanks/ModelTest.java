6449bfaf5755676e5c8db0eac02d401323419158
ggnmstr <jora.babayan@gmail.com>
2023-04-27 09:02:42 +0700

    first working test

--- lab2/src/test/java/com/github/ggnmstr/tanks/ModelTest.java
+++ lab2/src/test/java/com/github/ggnmstr/tanks/ModelTest.java
@@ -3,8 +3,9 @@ package com.github.ggnmstr.tanks;
 import com.github.ggnmstr.tanks.model.BattleField;
 import com.github.ggnmstr.tanks.util.Direction;
 import com.github.ggnmstr.tanks.util.FieldParameters;
-import junit.framework.TestCase;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ModelTest {
@@ -14,10 +15,23 @@ public class ModelTest {
         BattleField field = new BattleField(FieldParameters.getDefaultParameters());
         field.initField();
         field.moveMainPlayer(Direction.RIGHT);
+        for (int i = 0; i < 300; i++){
+            field.updateField();
+        }
         field.shootTank();
+        for (int i = 0; i < 300; i++){
+            field.updateField();
+        }
         field.shootTank();
+        for (int i = 0; i < 300; i++){
+            field.updateField();
+        }
         field.shootTank();
-        //TestCase.assertEquals();
-
+        NullPointerException exception = assertThrows(NullPointerException.class,()->{
+            for (int i = 0; i < 300; i++){
+                field.updateField();
+            }
+        },"abc");
+        assertTrue(exception.getMessage().contains("\"this.fieldListener\" is null"));
     }
 }
